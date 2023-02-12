2837c6068822580ab1300757dab93e7310854b63
A-oada <abdulrahmanoada@gmail.com>
2023-02-12 14:55:59 +0000

    Sun, Feb 12, 2023, 2:55 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -21,6 +21,18 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 }
 
 int compute_score(string word)
