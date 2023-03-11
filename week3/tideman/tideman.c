8c8bc29962281e21d8ce95936d15e472ce670664
A-oada <abdulrahmanoada@gmail.com>
2023-03-11 22:05:28 +0000

    Sat, Mar 11, 2023, 10:05 PM +00:00

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -116,20 +116,21 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // Row is determined by i, ith element in ranks will determine the row that represents the most preferred candidate
-    for (int i = 0; i < candidates_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        if (int i == ranks[i])
+        if (i == ranks[i])
         {
-            for (int j = 0; j < candidates_count; j++)
+            for (int j = 0; j < candidate_count; j++)
             {
                 // If the ith elemen in ranks precedes the jth element then it is most preferred, therefore add edge
                 if (j > i)
                 {
                     preferences[i][j] = 1;
                 }
+                i = 0;
             }
             // Reset i to check ranks to avoid skipping elements of ranks
-            int i = 0;
+
         }
     }
     return;
