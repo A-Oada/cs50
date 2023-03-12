d0d5ed5b14b639d14c1ce5d834a8648d92a374f2
A-oada <abdulrahmanoada@gmail.com>
2023-03-12 15:43:14 +0000

    Sun, Mar 12, 2023, 3:43 PM +00:00

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -121,7 +121,7 @@ void record_preferences(int ranks[])
         {
             if (ranks[i] < ranks[j])
             {
-                preferences[i][j] += 1;
+                preferences[i][j]++;
             }
         }
     }
