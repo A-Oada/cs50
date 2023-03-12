d9bc7b8e8c2c1df3b9dd7cd62ac3c027aad1b145
A-oada <abdulrahmanoada@gmail.com>
2023-03-12 19:31:43 +0000

    Sun, Mar 12, 2023, 7:31 PM +00:00

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -165,24 +165,16 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    int strength[pair_count][2];
-    for (int i = 0; i < pair_count; i++)
-    {
-        strength[i][0] = i;
-        int j = i + 1;
-        strength[i][1] = abs(preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner]);
-    }
+    // Store the differences between winner and loser in each pair in an array
+    // The second index denotes the number of the pair in pairs
+    // For example, if strength[0][0] = 0, strength[0][1] = 5
+    // This means that element[0] in strength is  pairs[2] and the gap between loser and winner is 5
     for (int i = 0; i < pair_count; i++)
     {
         for (int j = 0; j < pair_count; j++)
         {
-            int temp;
-            if (strength[i][1] > strength[j][1])
-            {
-                temp = strength[j][1];
-                strength[j][1] = strength[i][1];
-                strength[i][1] = temp;
-            }
+            pair temp;
+            pairs[]
         }
     }
     return;
