49244758df2b4c23355142feb0cf4a9cb8980471
A-oada <abdulrahmanoada@gmail.com>
2023-03-19 02:43:13 +0000

    Sun, Mar 19, 2023, 2:43 AM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -33,14 +33,14 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Copy header from input file to output file
     // Initialize the buffer that will contain the header data
     uint8_t header[44];
     // Read and write the info into the header file
     fread(header, sizeof(uint8_t), HEADER_SIZE, input);
     fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
-    // TODO: Read samples from input file and write updated data to output file
+    // Read samples from input file and write updated data to output file
     int16_t buffer;
     while (fread(&buffer, sizeof(int16_t), 1, input) == 1)
     {
