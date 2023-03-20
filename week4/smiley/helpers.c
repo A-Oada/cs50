f9b7382d1c0461e81ec2f4258620340ef630c983
A-oada <abdulrahmanoada@gmail.com>
2023-03-20 22:36:52 +0000

    Mon, Mar 20, 2023, 10:36 PM +00:00

--- week4/smiley/helpers.c
+++ week4/smiley/helpers.c
@@ -6,10 +6,10 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if (image[i][j].rgbtRED == 0x00 && image[i][j].rgbtGREEN == 0x00
-                && image[i][j].rgbtBLUE == 0x00)
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00
+                && image[i][j].rgbtBlue == 0x00)
                 {
-                    image[i][j].rgbtBLUE == 0xff;
+                    image[i][j].rgbtBlue = 0xff;
                 }
         }
     }
