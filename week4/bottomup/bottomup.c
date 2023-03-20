bf7fac6db3ee6227d1f43a6cf1400f7aa757ad34
A-oada <abdulrahmanoada@gmail.com>
2023-03-20 00:51:38 +0000

    Mon, Mar 20, 2023, 12:51 AM +00:00

--- week4/bottomup/bottomup.c
+++ week4/bottomup/bottomup.c
@@ -1,99 +1,98 @@
-// Copies a BMP file
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "bmp.h"
 
 int main(int argc, char *argv[])
 {
-    // Ensure proper usage
+    // ensure proper usage
     if (argc != 3)
     {
-        printf("Usage: copy infile outfile\n");
+        fprintf(stderr, "Usage: ./bottomup infile outfile\n");
         return 1;
     }
 
-    // Remember filenames
+    // remember filenames
     char *infile = argv[1];
     char *outfile = argv[2];
 
-    // Open input file
+    // open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
-        printf("Could not open %s.\n", infile);
+        fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
 
-    // Open output file
+    // open output file
     FILE *outptr = fopen(outfile, "w");
     if (outptr == NULL)
     {
         fclose(inptr);
-        printf("Could not create %s.\n", outfile);
+        fprintf(stderr, "Could not create %s.\n", outfile);
         return 3;
     }
 
-    // Read infile's BITMAPFILEHEADER
+    // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
 
-    // Read infile's BITMAPINFOHEADER
+    // read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
-    // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
         fclose(outptr);
         fclose(inptr);
-        printf("Unsupported file format.\n");
+        fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
 
-    // Write outfile's BITMAPFILEHEADER
+    // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
-    // Write outfile's BITMAPINFOHEADER
+    // write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
-    // Determine padding for scanlines
+    // determine padding for scanlines
     int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
-    // Iterate over infile's scanlines
-    for (int i = abs(bi.biHeight) - 1; i >= 0; i--)
+    // iterate over infile's scanlines
+    for (int i = bi.biHeight - 1; i >= 0; i--)
     {
-        // Iterate over pixels in scanline
+        // iterate over pixels in scanline
         for (int j = 0; j < bi.biWidth; j++)
         {
-            // Temporary storage
+            // temporary storage
             RGBTRIPLE triple;
 
-            // Read RGB triple from infile
+            // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            // Write RGB triple to outfile
+            // write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
         }
 
-        // Skip over padding, if any
+        // skip over padding, if any
         fseek(inptr, padding, SEEK_CUR);
 
-        // Then add it back (to demonstrate how)
+        // then add it back (to demonstrate how)
         for (int k = 0; k < padding; k++)
         {
             fputc(0x00, outptr);
         }
     }
 
-    // Close infile
+    // close infile
     fclose(inptr);
 
-    // Close outfile
+    // close outfile
     fclose(outptr);
 
-    // Success
+    // success
     return 0;
 }
