7265195003468951817ce296213b0cdfda5b675c
A-oada <abdulrahmanoada@gmail.com>
2023-03-24 05:28:38 +0000

    Fri, Mar 24, 2023, 5:28 AM +00:00

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -1,9 +1,11 @@
 #include "helpers.h"
 #include <math.h>
 
-// Convert image to grayscale
-// For each pixel, turn it into grayscale
-// Grayscale is calculate by approximating the avera
+// Convert image to grayscale.
+// For each pixel, turn it into grayscale.
+// Grayscale is calculated by approximating the average of all
+// colors in a pixel and rounding that number
+// Values of r, g and b are cast into floats to accurately round the numbers
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     float gray;
