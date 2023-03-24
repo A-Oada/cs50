3a07e42a3e75a7c2ca4f0958de8bc63dba17d3cc
A-oada <abdulrahmanoada@gmail.com>
2023-03-24 04:55:28 +0000

    Fri, Mar 24, 2023, 4:55 AM +00:00

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -4,6 +4,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            rgbtBlue.image[i][j] = round(255/2);
+        }
+    }
     return;
 }
 
