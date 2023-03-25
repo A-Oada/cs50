f436a7b08cc7e558c18c80905b7975b651388477
A-oada <abdulrahmanoada@gmail.com>
2023-03-25 05:31:35 +0000

    Sat, Mar 25, 2023, 5:31 AM +00:00

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -42,7 +42,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     // Copy right half of image into left half
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < round((float)width/2); j++)
         {
             image[i][j].rgbtRed = image[i][width - j].rgbtRed;
             image[i][j].rgbtGreen = image[i][width - j].rgbtGreen;
